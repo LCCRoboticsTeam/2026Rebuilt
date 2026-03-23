6c48be9f18ed705a429507e6c535a7679f09e344
djsandtoes <dave@sandtoes.net>
2026-03-23 07:31:55 -0700

    Updates after 2026 SD District

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -55,7 +55,7 @@ public final class Constants {
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 2.5;  // Orig 4.8, 2024Crecendo 4.0, 2025Reefscape 3.75, 2026Rebuilt trying 2.5
+    public static final double kMaxSpeedMetersPerSecond = 3.75;  // Orig 4.8, 2024Crecendo 4.0, 2025Reefscape 3.75, 2026Rebuilt trying 2.5
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
     public static final double kDirectionalSlewRate = 4; // radians per second; was .6
@@ -220,7 +220,7 @@ public final class Constants {
     ARM_DOWN_POSITION(-10);
 
     private double armPosition;
-    armState(double armPosition) {
+    armState(double armPostion) {
       this.armPosition = armPosition;
     }
     armState() {}
